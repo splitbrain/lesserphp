fd6f670a63c5d56f65522eca3e173f9d7b34105c
Andreas Gohr <andi@splitbrain.org>
2024-01-27 10:13:48 +0100

    Apply PHPCS rules
    This applies the same rules as defined in
    6ad7de0553bee8a2cbb81efca5df43430311772d by the ca-dsgn fork.

--- tests/ErrorHandlingTest.php
+++ tests/ErrorHandlingTest.php
@@ -4,68 +4,68 @@ require_once __DIR__ . "/../lessc.inc.php";
 use PHPUnit\Framework\TestCase;
 
 class ErrorHandlingTest extends TestCase {
-	public function setUp(): void {
-		$this->less = new lessc();
-	}
+    public function setUp(): void {
+        $this->less = new lessc();
+    }
 
-	public function compile() {
-		$source = join("\n", func_get_args());
-		return $this->less->compile($source);
-	}
+    public function compile() {
+        $source = join("\n", func_get_args());
+        return $this->less->compile($source);
+    }
 
     public function testRequiredParametersMissing() {
         $this->expectException(Exception::class);
         $this->expectExceptionMessage(".parametric-mixin is undefined");
         $this->compile(
-			'.parametric-mixin (@a, @b) { a: @a; b: @b; }',
-			'.selector { .parametric-mixin(12px); }'
-		);
-	}
+            '.parametric-mixin (@a, @b) { a: @a; b: @b; }',
+            '.selector { .parametric-mixin(12px); }'
+        );
+    }
 
     public function testTooManyParameters() {
         $this->expectExceptionMessage(".parametric-mixin is undefined");
         $this->expectException(Exception::class);
         $this->compile(
-			'.parametric-mixin (@a, @b) { a: @a; b: @b; }',
-			'.selector { .parametric-mixin(12px, 13px, 14px); }'
-		);
-	}
+            '.parametric-mixin (@a, @b) { a: @a; b: @b; }',
+            '.selector { .parametric-mixin(12px, 13px, 14px); }'
+        );
+    }
 
     public function testRequiredArgumentsMissing() {
         $this->expectExceptionMessage("unrecognised input");
         $this->expectException(Exception::class);
         $this->compile('.selector { rule: e(); }');
-	}
+    }
 
     public function testVariableMissing() {
         $this->expectExceptionMessage("variable @missing is undefined");
         $this->expectException(Exception::class);
         $this->compile('.selector { rule: @missing; }');
-	}
+    }
 
     public function testMixinMissing() {
         $this->expectExceptionMessage(".missing-mixin is undefined");
         $this->expectException(Exception::class);
         $this->compile('.selector { .missing-mixin; }');
-	}
+    }
 
     public function testGuardUnmatchedValue() {
         $this->expectExceptionMessage(".flipped is undefined");
         $this->expectException(Exception::class);
         $this->compile(
-			'.flipped(@x) when (@x =< 10) { rule: value; }',
-			'.selector { .flipped(12); }'
-		);
-	}
+            '.flipped(@x) when (@x =< 10) { rule: value; }',
+            '.selector { .flipped(12); }'
+        );
+    }
 
     public function testGuardUnmatchedType() {
         $this->expectExceptionMessage(".colors-only is undefined");
         $this->expectException(Exception::class);
         $this->compile(
-			'.colors-only(@x) when (iscolor(@x)) { rule: value; }',
-			'.selector { .colors-only("string value"); }'
-		);
-	}
+            '.colors-only(@x) when (iscolor(@x)) { rule: value; }',
+            '.selector { .colors-only("string value"); }'
+        );
+    }
 
     public function testMinNoArguments() {
         $this->expectExceptionMessage("expecting at least 1 arguments, got 0");
